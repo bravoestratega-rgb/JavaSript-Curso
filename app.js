0a86eba6728c6b09a253453bb2a90fadf6814109
Mauricio Fernandez Bravo <bravo.estratega@gmail.com>
2026-06-10 17:38:19 -0400

    Mi primer commit del curso

---
+++ app.js
@@ -0,0 +1,28 @@
+// console.log("Hola mundo");
+
+// creando variable por primera vez con let (forma nueva) o var (formar antigua), 
+
+let a = 10, 
+    b = 20, 
+    c = 30, 
+    d = 40; 
+    x = a + b;
+
+/* Distintos tipos de console, hay más? investigar
+console.log(x);
+console.warn(x);
+console.error(x);
+console.info(x);
+*/
+
+/* Poner nombre a las variables que aparecen en la consola
+
+console.log('a', a );
+console.log('b', b );
+console.log('c', c );
+
+*/
+
+console.log({ a });
+console.log({ b });
+console.log({ c });
