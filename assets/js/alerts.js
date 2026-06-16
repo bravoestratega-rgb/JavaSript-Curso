06b9f07ed6009802e3390f4d9947dba142d43cdd
Mauricio Fernandez <bravo.estratega@gmail.com>
2026-06-16 17:43:31 -0400

    Agregue palabras-reservadas.js

---
+++ assets/js/alerts.js
@@ -0,0 +1,10 @@
+//Tres tipos de ingreso de información del usuario:
+/*
+alert("¡Hola! Esto es una alerta.");
+
+let nombre = prompt("¿Cuál es tu nombre?");
+ console.log( nombre );
+ console.log("El nombre ingresado es: " + nombre);
+ */
+ const seleccion = confirm("¿Estás seguro de que quieres salir conmigo?"); 
+    console.log(seleccion);
---
+++ assets/js/app.js
@@ -0,0 +1,52 @@
+// console.log("Hola mundo");
+
+// creando variable por primera vez con let (forma nueva) o var (formar antigua), 
+alert('Hola desde app.js');
+
+
+let a = 10, 
+    b = 20, 
+    c = 'Hola ', 
+    d = 'Spiderman'; 
+    x = a + b;
+
+const saludo = c + d;
+
+
+/* Distintos tipos de console, hay más? investigar
+console.log(x);
+console.warn(x);
+console.error(x);
+console.info(x);
+*/
+
+/* Poner nombre a las variables que aparecen en la consola
+
+console.log('a', a );
+console.log('b', b );
+console.log('c', c );
+
+*/
+
+// {}}Las llaves indican que es un objeto, esto sirve para colocar el nombre y su valor
+// Si uno quiere elaborar visualmente su console puede usar css
+
+console.log('%c Mis variables', 'color:blue; font-weight: bold')
+console.log({ a });
+console.log({ b });
+console.log({ c });
+
+
+C = 'Hola de nuevo'
+
+console.table({a,b,c,d,x});
+
+/* console.table ayuda a ver los datos en una tabla
+
+console.table({a,b,c,d,x});
+
+*/
+
+
+
+
