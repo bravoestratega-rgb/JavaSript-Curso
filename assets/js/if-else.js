8f50499bff7368fcf0007596bbb5f0d10399e5dc
Mauricio Fernandez Bravo <bravo.estratega@gmail.com>
2026-07-17 09:31:22 -0400

    Hicimos proyecto día de la semana en consola

--- assets/js/if-else.js
+++ assets/js/if-else.js
@@ -32,3 +32,16 @@ if( dia === 0 ) {
 }
 
 
+let today = 6
+
+let diaHoy = {
+   0: 'domingo',
+   1: 'lunes',
+   2: 'martes',
+   3: 'miercoles',
+   4: 'jueves',
+   5: 'viernes',
+   6: 'sabado',
+};
+
+console.log( diaHoy[today] || 'Día no definido');
